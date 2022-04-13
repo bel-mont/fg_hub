f19482f6e2a9871b1c196337c7c97e01a78252bb
bel-mont <bel-mont@users.noreply.github.com>
2022-04-13 19:50:35 +0900

    Tailwind test init

---
+++ frontend/admin/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        mdr: '0 4px 15px -5px rgb(0 0 0 / 0.25)'
+      }
+    },
+  },
+  plugins: [],
+}
