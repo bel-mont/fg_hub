830ec5a9dad8cff26838236756925d56184285c7
bel-mont <cidhighwind85@hotmail.com>
2022-08-01 22:05:24 +0900

    fix: resource typo

--- infrastructure/fghub/modules/containers/ecs-service.tf
+++ infrastructure/fghub/modules/containers/ecs-service.tf
@@ -1,22 +1,22 @@
-wresource "aws_ecs_service" "fghub-web" {
+resource "aws_ecs_service" "fghub-web" {
   name            = "fghub-web-${var.ENV}"
   cluster         = aws_ecs_cluster.fghub-cluster.id
   task_definition = aws_ecs_task_definition.fghub-web.arn
-#  execution_role_arn = aws_iam_role.ecs-task-execution-role.arn
-#  task_role_arn      = aws_iam_role.ecs-demo-task-role.arn
+  #  execution_role_arn = aws_iam_role.ecs-task-execution-role.arn
+  #  task_role_arn      = aws_iam_role.ecs-demo-task-role.arn
   desired_count   = 1
   launch_type     = "FARGATE"
   network_configuration {
     subnets = var.PUBLIC_SUBNETS
   }
   #  iam_role        = aws_iam_role.foo.arn
-#    depends_on      = [aws_iam_role_policy.foo]
+  #    depends_on      = [aws_iam_role_policy.foo]
 
-    load_balancer {
-      target_group_arn = aws_alb_target_group.fghub-web-app.arn
-      container_name   = "fghub-web-app-${var.ENV}"
-      container_port   = 80
-    }
+  load_balancer {
+    target_group_arn = aws_alb_target_group.fghub-web-app.arn
+    container_name   = "fghub-web-app-${var.ENV}"
+    container_port   = 80
+  }
   #
   #  placement_constraints {
   #    type       = "memberOf"
